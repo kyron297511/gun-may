9510723adf363e546249181cc27588949ecc5761
jackyyzhang@gmail.com <jackyyzhang@gmail.com>
2021-06-10 14:31:12 -0400

    Added controls to the launcher

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-from tkinter import *
+import tkinter as tk
+from tkinter.ttk import *
 import game
 
 
-class Launcher(Tk):
+class Launcher(tk.Tk):
     def __init__(self):
         super().__init__()       
         
         self.title("Gun Mayhem Launcher")
 
-        # canvas = Canvas(width=531, height=76)
-        # canvas.grid(row=0, column=0)
-        # self.title = PhotoImage(file="assets/launcher/title.gif")
-        # canvas.create_image(0, 0, anchor=NW, image=self.title)
+        canvas = tk.Canvas(width=531, height=76)
+        canvas.grid(row=0, column=0, columnspan=2, padx=20, pady=15)
+        self.title = tk.PhotoImage(file="assets/launcher/title.gif")
+        canvas.create_image(0, 0, anchor="nw", image=self.title)
+
+        Label(text="Player 1 controls:").grid(row=1, column=0)
+        
+        Label(text="Move left: ←").grid(row=2, column=0)
+        Label(text="Move right: →").grid(row=3, column=0)
+        Label(text="Jump: ↑").grid(row=4, column=0)
+        Label(text="Shoot: .").grid(row=5, column=0)
+        
+        Label(text="Player 2 controls:").grid(row=1, column=1)
+        Label(text="Move left: A").grid(row=2, column=1)
+        Label(text="Move right: D").grid(row=3, column=1)
+        Label(text="Jump: W").grid(row=4, column=1)
+        Label(text="Shoot: H").grid(row=5, column=1)
+
+        Separator(orient='horizontal').grid(row=6, column=0, columnspan=2, ipadx=250, pady=10)
 
         color_options = [
             "Black",
@@ -21,38 +37,35 @@ class Launcher(Tk):
             "Yellow",
         ]
 
-        Label(text="Enter the name for Player 1:").grid(row=0, column=0)
-        Label(text="Enter the name for Player 2:").grid(row=1, column=0)
+        Label(text="Enter the name for Player 1:").grid(row=7, column=0)
+        Label(text="Enter the name for Player 2:").grid(row=7, column=1)
         self.player_1_name_input = Entry()
         self.player_2_name_input = Entry()
         self.player_1_name_input.insert(10, "Player 1")
         self.player_2_name_input.insert(10, "Player 2")
-        self.player_1_name_input.grid(row=0, column=1)
-        self.player_2_name_input.grid(row=1, column=1)
+        self.player_1_name_input.grid(row=8, column=0)
+        self.player_2_name_input.grid(row=8, column=1)
 
-        self.player_1_color_variable = StringVar()
-        self.player_1_color_variable.set(color_options[0])
+        self.player_1_color_variable = tk.StringVar()
+        self.player_2_color_variable = tk.StringVar()
 
-        self.player_2_color_variable = StringVar()
-        self.player_2_color_variable.set(color_options[0])
-
-        Label(text="Choose the color for Player 1:").grid(row=2, column=0)
+        Label(text="Choose the color for Player 1:").grid(row=9, column=0, pady=10)
         self.player_1_color_input = OptionMenu(
-            self, self.player_1_color_variable, *color_options)
-        self.player_1_color_input.grid(row=2, column=1, sticky="w")
+            self, self.player_1_color_variable, color_options[2], *color_options)
+        self.player_1_color_input.grid(row=10, column=0)
         self.player_1_color_input.configure(width=15)
 
-        Label(text="Choose the color for Player 2:").grid(row=3, column=0)
+        Label(text="Choose the color for Player 2:").grid(row=9, column=1, pady=10)
         self.player_2_color_input = OptionMenu(
-            self, self.player_2_color_variable, *color_options)
-        self.player_2_color_input.grid(row=3, column=1, sticky="w")
+            self, self.player_2_color_variable, color_options[3], *color_options)
+        self.player_2_color_input.grid(row=10, column=1)
         self.player_2_color_input.configure(width=15)
 
         Button(text="Launch", command=self.run_game).grid(
-            row=4, column=0, sticky="e")
+            row=11, column=0, sticky="e", pady=10)
 
         Button(text="Exit", command=self.quit).grid(
-            row=4, column=1, sticky="w")
+            row=11, column=1, sticky="w", pady=10)
 
     def get_input(self):
         player_1_name = self.player_1_name_input.get()
